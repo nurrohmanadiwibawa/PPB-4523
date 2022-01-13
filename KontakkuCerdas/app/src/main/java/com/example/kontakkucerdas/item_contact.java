ecb2df41c233e908921dde43a3b8de44e6bee724
nurrohmanadiwibawa <91300067+nurrohmanadiwibawa@users.noreply.github.com>
2022-01-13 23:37:29 +0700

    UAS

---
+++ KontakkuCerdas/app/src/main/java/com/example/kontakkucerdas/item_contact.java
@@ -0,0 +1,14 @@
+package com.example.kontakkucerdas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class item_contact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_contact);
+    }
+}
