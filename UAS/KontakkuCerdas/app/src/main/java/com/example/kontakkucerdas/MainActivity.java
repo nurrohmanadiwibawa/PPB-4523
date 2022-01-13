dfb40f16852a864ba26373bab4065948c9b996db
nurrohmanadiwibawa <91300067+nurrohmanadiwibawa@users.noreply.github.com>
2022-01-13 23:39:54 +0700

    UAS

---
+++ UAS/KontakkuCerdas/app/src/main/java/com/example/kontakkucerdas/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.kontakkucerdas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ActivityTambahContact.class));
+
+            }
+        });
+    }
+}
